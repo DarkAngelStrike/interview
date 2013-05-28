82a6395b803576d564609cd8aab854d6b212371e
DarkAngelStrike <darkangelstrike@hotmail.com>
2013-05-28 11:34:55 -0300

    Update interview.py
    Porting interview.py from Python2 to Python3.

--- interview.py
+++ interview.py
@@ -1,60 +1,56 @@
-#!/usr/bin/python
-
-# This software can be freely modified, embedded and redistributed under LGPL.
-# http://www.gnu.org/copyleft/lesser.html
-# Copyright Kazuki Nakajima <nkjm.kzk@gmail.com>
+#!/usr/bin/python3
 
 import string
 import sys
 
 class Interview:
-    def interview(self, question, default, choice_list): 
+    def interview(self, question, default, choice_list):
         if (choice_list is None):
             if not default is None and default != '':
-                print "Please enter %s (just Enter to accept default '%s'): " % (question, default),
+                print ("Please enter %s (just Enter to accept default '%s'): " % (question, default),)
             else:
-                print "Please enter %s: " % question,
+                print ("Please enter %s: " % question,)
 
             for line in iter(sys.stdin.readline, ""):
                 answer = line.replace("\n","")
                 if not (default is None) and (answer == ""):
-                    print ""
+                    print ("")
                     return default
                 elif (default is None) and (answer == ""):
-                    print ""
-                    print "Please enter %s: " % question,
+                    print ("")
+                    print ("Please enter %s: " % question,)
                     continue
                 break
-            print ""
+            print ("")
             return answer
         else:
-            print "Please select %s from following list. (Enter the number)" % question
+            print ("Please select %s from following list. (Enter the number)" % question)
             if not default is None and default != '':
-                print "(just Enter to accept default '%s')" % default
+                print ("(just Enter to accept default '%s')" % default)
             offset = 0
             for choice in choice_list:
-                print "\t[%s] %s" % (offset, choice)
+                print ("\t[%s] %s" % (offset, choice))
                 offset = offset + 1
-            print "Answer: ",
+            print ("Answer: ",)
             for line in iter(sys.stdin.readline, ""):
                 answer = line.replace("\n","")
                 if not (default is None) and (answer == ""):
-                    print ""
+                    print ("")
                     return default
                 else:
                     try:
                         answer = int(answer)
                     except:
-                        print "Invalide answer. Please enter the NUMBER [0-%s]." % (offset - 1)
-                        print "Answer: ",
+                        print ("Invalide answer. Please enter the NUMBER [0-%s]." % (offset - 1))
+                        print ("Answer: ",)
                         continue
 
                 if (int(answer) >= 0) and (int(answer) < offset):
-                    print ""
+                    print ("")
                     return (choice_list[int(answer)])
                 else:
-                    print "Invalide answer. Please enter the NUMBER [0-%s]." % (offset - 1)
-                    print "Answer: ",
+                    print ("Invalide answer. Please enter the NUMBER [0-%s]." % (offset - 1))
+                    print ("Answer: ",)
 
     def ask_yes_or_no(self):
         for line in iter(sys.stdin.readline, ""):
@@ -63,7 +59,7 @@ class Interview:
             elif (line == "n\n"):
                 return "no"
             else:
-                print "Enter just 'y' or 'n'. [y/n]: ",
+                print ("Enter just 'y' or 'n'. [y/n]: ",)
 
     def ask_new_name(self, question, input, default, choice_list=None):
         if input is None:
@@ -80,12 +76,12 @@ class Interview:
             elif isinstance(choice_list, list) or isinstance(choice_list, tuple) and name not in choice_list:
                 break
             elif isinstance(choice_list, list) or isinstance(choice_list, tuple) and name in choice_list:
-                print "Specified NEW %s already exist. Please try another name." % question
+                print ("Specified NEW %s already exist. Please try another name." % question)
             elif not isinstance(choice_list, list) and not isinstance(choice_list, tuple):
-                print "choice_list is not neither list nor tuple."
+                print ("choice_list is not neither list nor tuple.")
                 sys.exit()
             else:
-                print "Unexpected situation."
+                print ("Unexpected situation.")
                 sys.exit()
             try:
                 name = self.interview(question=question, choice_list=None, default=default)
@@ -102,10 +98,10 @@ class Interview:
         else:
             name = input
             if not isinstance(choice_list, list) and not isinstance(choice_list, tuple):
-                print "choice_list is not neither list nor tuple."
+                print ("choice_list is not neither list nor tuple.")
                 sys.exit()
             while not name in choice_list:
-                print "%s is not valid." % question
+                print ("%s is not valid." % question)
                 try:
                     name = self.interview(question=question, choice_list=choice_list, default=default)
                 except:
@@ -127,7 +123,7 @@ class Interview:
             elif number.isdigit() and int(number) > 0:
                 break
             else:
-                print "%s is not valid." % question
+                print ("%s is not valid." % question)
             try:
                 number = self.interview(question=question, choice_list=None, default=default)
             except:
@@ -136,7 +132,7 @@ class Interview:
 
     def ask_list_from_list(self, question, input, default, choice_list):
         if not isinstance(choice_list, list) and not isinstance(choice_list, tuple):
-            print "choice_list is not neither list nor tuple."
+            print ("choice_list is not neither list nor tuple.")
             sys.exit()
         dynamic_list = list(choice_list)
         name_list = []
@@ -149,10 +145,10 @@ class Interview:
                 break
             if not name in name_list:
                 name_list.append(name)
-            print "Selected %s:" % question
+            print ("Selected %s:" % question)
             for i in name_list:
-                print "\t" + i
-            print ""
+                print ("\t" + i)
+            print ("")
             try:
                 dynamic_list.remove("That's it.")
             except:
@@ -160,4 +156,3 @@ class Interview:
             if len(dynamic_list) < 1:
                 break
         return name_list
-
